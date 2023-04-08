d5ec57a218e9792fdf2d6364175ac07f49055db2
subcylindrical <zacharylehouillier@gmail.com>
2023-04-08 10:33:11 -0700

    add addToCart feature

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,13 +6,13 @@ import { CartContext } from '../../contexts/cart.context';
 
 const CartIcon = () => {
   const cart = useContext(CartContext);
-  const { isCartOpen, setIsCartOpen } = cart;
+  const { isCartOpen, setIsCartOpen, cartItems, cartCount } = cart;
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className='cart-icon-container' onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>10</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   );
 };
